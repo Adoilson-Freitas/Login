88cd7ca9c2aa901129b0ec28b46405d8e6b6c5da
Adoilson-Freitas <adoilsonbacelar@gmail.com>
2019-10-24 23:51:17 -0300

    Criando telas de login e cadastro com Html e CSS

---
+++ cadastrar.php
@@ -0,0 +1,21 @@
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <title>Tela Login</title>
+    <link rel="stylesheet" href="CSS/estilo.css">
+</head>
+<body>
+<div id="corpo-form-Cad">
+	<h1>Cadastrar</h1>
+    <form method="post" action="processa.php">
+        <input type="text" placeholder="Nome Completo">
+        <input type="text" placeholder="Telefone">
+        <input type="email" placeholder="Usuário">
+        <input type="password" placeholder="Senha">
+        <input type="password" placeholder="Confirmar Senha">
+        <input type="submit" value="Cadastrar">
+    </form>
+   </div>
+</body>
+
+</html>
