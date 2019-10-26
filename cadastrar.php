801ca4974e4e75e680d4d3533a5ced8f86171a0d
Adoilson-Freitas <adoilsonbacelar@gmail.com>
2019-10-25 23:40:07 -0300

    funções para conectar no BD, cadastrar e logar

--- cadastrar.php
+++ cadastrar.php
@@ -1,3 +1,8 @@
+<?php 
+    require_once 'CLASSES/usuarios.php';
+    $u = new Usuario;
+ ?>
+
 <html lang="pt-br">
 <head>
     <meta charset="utf-8" />
@@ -7,15 +12,60 @@
 <body>
 <div id="corpo-form-Cad">
 	<h1>Cadastrar</h1>
-    <form method="post" action="processa.php">
-        <input type="text" placeholder="Nome Completo">
-        <input type="text" placeholder="Telefone">
-        <input type="email" placeholder="Usuário">
-        <input type="password" placeholder="Senha">
-        <input type="password" placeholder="Confirmar Senha">
+    <form method="POST">
+        <input type="text" name="nome" placeholder="Nome Completo" maxlength="30">
+        <input type="text" name="telefone" placeholder="Telefone" maxlength="30">
+        <input type="email" name="email" placeholder="Usuário" maxlength="40">
+        <input type="password" name="senha" placeholder="Senha" maxlength="15">
+        <input type="password" name="confsenha" placeholder="Confirmar Senha" maxlength="15">
         <input type="submit" value="Cadastrar">
     </form>
-   </div>
+</div>
+<?php 
+//verificar se clicou no botão
+if(isset($_POST['nome']))
+{
+    $nome = addslashes($_POST['nome']);
+    $telefone = addslashes($_POST['telefone']);
+    $email = addslashes($_POST['email']);
+    $senha = addslashes($_POST['senha']);
+    $confirmarsenha = addslashes($_POST['confsenha']);
+    //verificar se está vazio 
+    if(!empty($nome) && !empty($telefone) && !empty($email) && !empty($senha) && !empty($confirmarsenha))
+    {
+        $u->conectar("projeto_login","localhost","root","");
+        if($u->msgErro == "")//todo ok
+        {
+            if($senha == $confirmarsenha)
+            {
+                 if($u->cadastrar($nome,$telefone,$email,$senha))
+                 {
+                    echo "Cadastrado com sucesso! Acesse para entrar!";
+                 }
+                 else
+                 {
+                    echo "Email já cadastrado!";
+                 }
+            }
+            else
+            {
+                echo "Senha e Confirmar senha não correspondem!";
+            }
+            
+        }
+        else
+        {
+            echo "Erro: ".$u->msgErro;
+        }
+    }
+    else
+    {
+        echo "preencha todos os campos!";
+    }
+
+}
+
+ ?>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -7,13 +7,18 @@
 <body>
 <div id="corpo-form">
 	<h1>Entrar</h1>
-    <form method="post" action="processa.php">
-        <input type="email" placeholder="Usuário">
-        <input type="password" placeholder="Senha">
+    <form method="post">
+        <input type="email" placeholder="Usuário" name="email">
+        <input type="password" placeholder="Senha" name="senha">
         <input type="submit" value="ACESSAR">
         <a href="cadastrar.php">Ainda não é inscrito?<strong>Cadastre-se!</strong></a>
     </form>
-   </div>
+</div>
+<?php 
+
+
+
+?>
 </body>
 
 </html>
